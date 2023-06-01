1423bb7356ba6eaba233e128135cc41ac8a70022
davilivob <david.iii.chiu@gmail.com>
2023-06-02 00:57:09 +0800

    slider value string to int

--- dist/public/js/index.js
+++ dist/public/js/index.js
@@ -9,8 +9,8 @@ for (let i = 1; i <= slider_ammount; i++) {
                 'Content-Type': 'application/json'
             },
             body: JSON.stringify({
-                slider_1: document.getElementById('slider_1').value,
-                slider_2: document.getElementById('slider_2').value
+                slider_1: parseInt(document.getElementById('slider_1').value),
+                slider_2: parseInt(document.getElementById('slider_2').value)
             })
         }).then(() => {
             const slider_value = document.getElementById(`slider_${i}_value`);
